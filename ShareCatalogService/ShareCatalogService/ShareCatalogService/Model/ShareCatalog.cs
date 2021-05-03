ee3f5f70b82be5cac4bc16ac7dbbcca208f26bfd
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-05-03 15:50:03 +0200

    ShareCatalog service

---
+++ ShareCatalogService/ShareCatalogService/ShareCatalogService/Model/ShareCatalog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareCatalogService.Model
+{
+	public class ShareCatalog
+	{
+
+		public string Name { get; set; }
+
+		public string Id {get; set;}
+
+		public User Owner {get; set;}
+
+		public float Value {get; set;}
+
+		public float Tax {get; set;}
+
+		public bool ForSale {get; set;}
+
+	}
+
+
+	public class User
+	{
+		public string Id {get; set;}
+
+		public string Name {get; set;}
+
+	}
+}
