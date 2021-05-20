ead083e912167ff49a689b3e8342a80833e7177c
Quynh Trang Mac <201810269@post.au.dk>
2021-05-20 15:58:00 +0200

    add buyer-service

---
+++ BuyShareService/Properties/BuyerController.cs
@@ -0,0 +1,62 @@
+﻿using BuyShareService.Clients;
+using BuyShareService.Controllers.Request;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BuyShareService.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuyerController : Controller
+    {
+        private readonly BrokerClient _brokerClient;
+        
+        public BuyerController(BrokerClient brokerClient)
+        {
+            _brokerClient = brokerClient;
+        }
+
+        [HttpPost("/buyShare")]
+        public async Task<ActionResult<BrokerRequest>> BuyShareRequester (string buyerId, string shareId) 
+        {
+
+            // Making a request to broker: 
+            try
+            {
+
+                var brokerRequest = new BrokerRequest
+                {
+                    BuyerId = buyerId,
+                    ShareId = shareId
+                };
+
+                await _brokerClient.BuyingRequest(brokerRequest);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception: ", e);
+            }
+
+            var frontendRequest = new BrokerRequest { BuyerId = buyerId, ShareId = shareId };
+            ActionResult<BrokerRequest> response = null;
+            try
+            {
+                response = await _brokerClient.FrontendResponse(frontendRequest);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception caught: ", e); ;
+            }
+
+            return Ok(frontendRequest);
+
+        }
+    }
+}
