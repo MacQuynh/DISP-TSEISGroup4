82e4d1377e83766247af4ce1c8f4bf66657f5106
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-05-31 11:15:31 +0200

    Added yml files

---
+++ TransactionService/TransactionService/Controllers/Requests/TransactionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionService.Controllers.Requests
+{
+    public class TransactionRequest
+    {
+        public string BuyerId { get; set; }
+        public string SellerId { get; set; }
+        public string ShareId { get; set; }
+        public double Tax { get; set; }
+        public double ShareValue { get; set; }
+        public double Price { get; set; }
+    }
+}
